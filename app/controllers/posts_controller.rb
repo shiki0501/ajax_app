0daeeca713d9d9923562495f98dda6bba1ad4b09
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-06-09 18:27:59 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.order(id: "DESC")
+    @posts = Post.all.order(id: "DESC")
   end
 
   # def new
@@ -9,6 +9,6 @@ class PostsController < ApplicationController
 
   def create
     Post.create(content: params[:content])
-    redirect_to action : :index
+    redirect_to action: :index
   end
 end
