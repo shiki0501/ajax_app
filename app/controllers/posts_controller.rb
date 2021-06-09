759821251daa6885ccecd2621d82931a6fef4a8a
shiki0501 <nyuuryoku.weruto2@gmail.com>
2021-06-09 18:42:20 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all.order(id: "DESC")
+    @posts = Post.order(id: "DESC")
   end
 
   # def new
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
 end
